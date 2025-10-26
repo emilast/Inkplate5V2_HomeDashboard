820f1061b771787944a15a70c85bfceba220bbe4
Emil Åström <emil_astrom@hotmail.com>
2025-10-26 14:07:30 +0100

    Handle daylight savings time automatically

--- src/Inkplate5V2_HomeDashboard.ino
+++ src/Inkplate5V2_HomeDashboard.ino
@@ -46,18 +46,15 @@ Inkplate display(INKPLATE_1BIT); // Create an object on Inkplate library and als
 #include "Fonts/AcariSansbd50.h"
 
 static const FontCollection fonts = {
-  AcariSans_Regular20pt8b,
-  AcariSans_Bold20pt8b,
-  AcariSans_Bold50pt7b,
-  AcariSans_Bold12pt8b
-};
+    AcariSans_Regular20pt8b,
+    AcariSans_Bold20pt8b,
+    AcariSans_Bold50pt7b,
+    AcariSans_Bold12pt8b};
 
 const char *networkName = "Inkplate5V2 Home Dashboard";
 
 // Time config
 const char *ntpServer = "pool.ntp.org";
-const long gmtOffset_sec = 3600;
-const int daylightOffset_sec = 3600;
 const int jsonBufferSize = 8192;
 
 // Trafiklab API details
@@ -71,8 +68,8 @@ const char *TrainType = "TRAIN";
 const char *BusType = "BUS";
 
 // Layout constants
-const int leftXpos = 35;    // Left column start position
-const int rightXpos = 430;  // right column start position
+const int leftXpos = 35;   // Left column start position
+const int rightXpos = 430; // right column start position
 
 const int row_height = 50;
 
@@ -100,17 +97,23 @@ void setup()
   {
     Serial.println("Connected to WiFi.");
   }
-  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+
+  // Get current time from time server
+  configTime(0, 0, ntpServer);
+
+  // Set timezone to CEST (Central European Summer Time)
+  setenv("TZ", "CET-1CEST,M3.5.0,M10.5.0/3", 1);
+  tzset();
 }
 
 void loop()
 {
   clearAndPrepareDisplay(display);
-  
+
   printLocalTime(display, fonts, leftXpos, 100);
-  
+
   drawWeather(display, fonts, leftXpos, 180, row_height, framesDrawn);
-  
+
   Serial.println("Bus information from API...");
   if (!drawDepartures(display, fonts, busSiteId, BusType, busTitle, rightXpos, 65, busLineWidth, true, row_height))
   {
@@ -121,7 +124,7 @@ void loop()
   {
     drawDepartures(display, fonts, trainSiteId, TrainType, trainTitle, rightXpos, 385, trainLineWidth, false, row_height);
   }
-  
+
   if (framesDrawn == 0)
   {
     display.display();
@@ -131,27 +134,28 @@ void loop()
     display.partialUpdate();
   }
   framesDrawn = (++framesDrawn % 10);
-  
+
   Serial.printf("Waiting for next update in %d seconds...\n", refreshInterval / 1000);
   delay(refreshInterval);
 }
 
-void printStartupMessage(Inkplate &display, const FontCollection &fonts, int xpos, int ypos) {
-    display.setCursor(xpos, ypos);
-    display.setFont(&fonts.normalTextFont);
-    display.println("Connecting to WiFi network...");
-    ypos += row_height;
+void printStartupMessage(Inkplate &display, const FontCollection &fonts, int xpos, int ypos)
+{
+  display.setCursor(xpos, ypos);
+  display.setFont(&fonts.normalTextFont);
+  display.println("Connecting to WiFi network...");
+  ypos += row_height;
 
-    display.setCursor(xpos, ypos);
-    // display.println("In case of problems, configure using the WiFi network:");
-    // ypos += row_height;
+  display.setCursor(xpos, ypos);
+  // display.println("In case of problems, configure using the WiFi network:");
+  // ypos += row_height;
 
-    int wrappedLines = drawWrappedText(display, fonts.normalTextFont, "In case of problems, configure the board using the temporary WiFi network:", xpos, ypos, E_INK_WIDTH - xpos - 25);
-    ypos += row_height * wrappedLines;
-    
-    display.setCursor(xpos, ypos);
-    display.setFont(&fonts.boldTextFont);
-    display.println(networkName);
+  int wrappedLines = drawWrappedText(display, fonts.normalTextFont, "In case of problems, configure the board using the temporary WiFi network:", xpos, ypos, E_INK_WIDTH - xpos - 25);
+  ypos += row_height * wrappedLines;
 
-    display.display();
+  display.setCursor(xpos, ypos);
+  display.setFont(&fonts.boldTextFont);
+  display.println(networkName);
+
+  display.display();
 }
